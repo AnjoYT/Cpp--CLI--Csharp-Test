336f285e564982ec37aa2467123659ec04e6cb8b
AnjoYT <AnjoYT@wp.pl>
2023-04-21 21:46:29 +0200

    Core
    Basic HelloWorld function returning text added

--- Core/Core.cpp
+++ Core/Core.cpp
@@ -1,6 +1,13 @@
 ﻿// Core.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
 //
-#include "myunmanagedclass.h"
-
 #include <iostream>
 #include <Windows.h>
+#include <string>
+#include "Core.h"
+
+namespace Core 
+{
+	std::string Hello::HelloWorld(std::string text) {
+		return text;
+	}
+}
---
+++ Core/Core.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+namespace Core
+{
+
+	class Hello 
+	{
+	public:
+		std::string HelloWorld(std::string text);
+	};
+}
