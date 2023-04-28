59ea361ee607a13aa848503a2552fde1f80b9e2e
AnjoYT <AnjoYT@wp.pl>
2023-04-28 12:20:19 +0200

    GUI refresh
    GUI refresh and final cleanup

--- HelloWpfApp/MainWindow.xaml.cs
+++ HelloWpfApp/MainWindow.xaml.cs
@@ -1,35 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using CLIwrapper;
 
 namespace HelloWpfApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        private bool isClicked = false;
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            HelloWrap nHello = new HelloWrap();
-            tb1.Text = nHello.Hello();
+            if (!isClicked)
+            {
+                HelloWrap nHello = new HelloWrap();
+                tb1.Text = nHello.Hello();
+                isClicked = true;
+            }
+            else 
+            { 
+                tb1.Text = "-";
+                isClicked = false;
+            }
 
         }
 
